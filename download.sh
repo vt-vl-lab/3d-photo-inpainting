6647d1e09bbf40d6fbc5de3f16ff0745e1a99175
at7788546@hotmail.com <at7788546@hotmail.com>
2020-07-23 03:40:55 -0400

    update download.sh to handle dead urls

--- download.sh
+++ download.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
+fb_status=$(wget --spider -S https://filebox.ece.vt.edu/ 2>&1 | grep  "HTTP/1.1 200 OK")
+
 mkdir checkpoints
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
+if [ -z "$fb_status" ]
+then
+    echo "filebox is down. Use alternative urls."
+    wget https://github.com/LemonATsu/3d-photo-model-weights/raw/master/color-model.pth
+    wget https://github.com/LemonATsu/3d-photo-model-weights/raw/master/depth-model.pth
+    wget https://github.com/LemonATsu/3d-photo-model-weights/raw/master/edge-model.pth
+    wget https://github.com/LemonATsu/3d-photo-model-weights/raw/master/model.pt
+else
+    echo "downloading from filebox ..."
+    wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
+    wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
+    wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
+    wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
+fi
+
 mv color-model.pth checkpoints/.
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
 mv depth-model.pth checkpoints/.
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
 mv edge-model.pth checkpoints/.
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
 mv model.pt MiDaS/.
