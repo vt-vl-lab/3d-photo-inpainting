841acbabede8515f1b7815a57e2564614e0d4720
LemonATSu <at7788546@gmail.com>
2021-06-21 11:44:42 -0400

    update download script

--- download.sh
+++ download.sh
@@ -4,10 +4,10 @@ fb_status=$(wget --spider -S https://filebox.ece.vt.edu/ 2>&1 | grep  "HTTP/1.1 
 mkdir checkpoints
 
 echo "downloading from filebox ..."
-#wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
-#wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
-#wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
-#wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
+wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
+wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
+wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
+wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
 
 mv color-model.pth checkpoints/.
 mv depth-model.pth checkpoints/.
@@ -18,8 +18,8 @@ echo "cloning from BoostingMonocularDepth ..."
 git clone https://github.com/compphoto/BoostingMonocularDepth.git
 mkdir -p BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/
 
-echo "downling from google drive ... (how?)"
+echo "downloading mergenet weights ..."
+wget https://sfu.ca/~yagiz/CVPR21/latest_net_G.pth
 mv latest_net_G.pth BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/
-#cd BoostingMonocularDepth/midas/
 wget https://github.com/intel-isl/MiDaS/releases/download/v2/model-f46da743.pt
 mv model-f46da743.pt BoostingMonocularDepth/midas/model.pt
