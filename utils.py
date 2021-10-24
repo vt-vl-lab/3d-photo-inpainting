13a325397d65ff433d9646b4a0f870c017c8bb70
Naelson Douglas <naelson17@gmail.com>
2021-10-24 14:23:35 -0300

    removed a literal caomparison pitfall from the code

--- utils.py
+++ utils.py
@@ -972,7 +972,7 @@ def follow_image_aspect_ratio(depth, image):
     return depth
 
 def depth_resize(depth, origin_size, image_size):
-    if origin_size[0] is not 0:
+    if origin_size[0] != 0:
         max_depth = depth.max()
         depth = depth / max_depth
         depth = resize(depth, origin_size, order=1, mode='edge')
