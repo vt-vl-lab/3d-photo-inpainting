45b4e3f5756af7ac37221c6eee3173990cd3f95e
donlinglok <6138806+donlinglok@users.noreply.github.com>
2023-06-13 16:12:48 +0800

    Update mesh_tools.py

--- mesh_tools.py
+++ mesh_tools.py
@@ -1,9 +1,6 @@
 import os
 import numpy as np
-try:
-    import cynetworkx as netx
-except ImportError:
-    import networkx as netx
+import networkx as netx
 
 import json
 import scipy.misc as misc
